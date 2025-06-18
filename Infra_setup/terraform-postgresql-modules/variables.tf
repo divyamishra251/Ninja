dff0a3e0600b183c56da77500d2d537ffc0073ac
divya <divya@pandey2510divya@gmail.com>
2025-06-18 18:22:35 +0530

    Updated Terraform provider block and region variable

--- Infra_setup/terraform-postgresql-modules/variables.tf
+++ Infra_setup/terraform-postgresql-modules/variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
